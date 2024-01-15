603f977e6e32cddcfe4d51e527d88589ac9deb45
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-01-15 13:07:31 -0500

    Moved application to its on folder

---
+++ sba308/README.md
@@ -0,0 +1,34 @@
+# CryptoInfo Web App
+
+## Table of Contents
+
+- [About](#about)
+- [Installing](#installing)
+- [Author](#author)
+
+
+## About <a name = "about"></a>
+
+Welcome to CryptoInfo, the platform built for comprehensive information on cryptocurrencies. My web application is designed to provide you with up-to-date details on various cryptocurrencies. 
+
+### Installing
+
+Clone Repository: Clone the CryptoInfo repository to your local machine.
+
+git clone https://github.com/MariyahW/PerScholas/tree/main/sba308
+
+Navigate to Project Directory: Change your working directory to the project folder.
+
+cd crypto-info
+
+Install Dependencies: Install the necessary dependencies.
+
+npm init -y
+npm i axios
+npm install chart.js
+
+### Author 
+
+MariyahW
+- [Github](https://github.com/MariyahW)
+- [Website](https://mariyahwatson.com)
---
+++ sba308/script.js
@@ -0,0 +1,48 @@
+
+import axios from "axios";
+
+import option from "./mod1.js";
+import *  as charting from  "./mod2.js";
+import * as pricing from "./mod3.js";
+
+const form = document.querySelector("select");
+const search = document.querySelector('#formButton');
+const item4 = document.getElementById('item4');
+const sect2 = document.getElementById('list-item-2');
+const api_key = `83afe97e-5f1a-495c-931d-45918a68d87c`;
+
+(async function fillForm() {
+  axios.get(`https://api.coincap.io/v2/assets`).then((response) => {
+    response.data.data.forEach((coin) => {
+      form.append(option(coin));
+    });
+  });
+})();
+
+form.addEventListener('change',fillLine);
+form.addEventListener('change',priceUsd);
+
+ function fillLine(event) {
+   event.preventDefault();
+   // item4.classList='active';
+   // console.log(item4.classList);
+   let coin = event.target.value;
+  axios.get(`https://api.coincap.io/v2/assets/${coin}/history?interval=d1`)
+  .then(res=>{
+   charting.clearChart();
+  charting.createLine(res);
+  
+  })
+ }
+ function priceUsd(event){
+   let coin = event.target.value;
+
+   axios.get(`https://api.coincap.io/v2/assets/${coin}`)
+   .then(response=>{
+      pricing.clearDiv(sect2);
+      sect2.append(pricing.getPrice(response));
+      sect2.append(pricing.getChange(response));
+      sect2.append(pricing.getSymbol(response));
+   
+   })
+ }
